acb5a303ba6da8ef5dc1d4ea10eba73ce8b58b9e
robogf <111620315+robogf@users.noreply.github.com>
2022-11-03 23:39:27 -0700

    finished product route and started catergory route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,13 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+
   // find all categories
+  try {
+  const categoryData = await Category.findAll({
+    include: [{model:Product}],
+  });
+ 
+} 
+catch (err) {
+  res.status(500).json(err);
+}
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async  (req, res) => { 
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model:Product}]
+    });
+    if (!categoryData){
+      res.status(404).json({ message: 'No category found with that id!'})
+    }
+  }catch (err) {
+  res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
@@ -21,8 +41,23 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+  
+    if(!categoryData){
+      res.status(404).json({message: 'No category found with this id'});
+      return;
+    }
+   res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
